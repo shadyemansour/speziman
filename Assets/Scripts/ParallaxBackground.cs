170b82bc41023db774b62052012f6ed1971cbdc5
valentinWild <74301124+valentinWild@users.noreply.github.com>
2024-06-29 13:48:50 +0200

    Fixed Script & created Prefab

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -6,31 +6,44 @@ using UnityEngine;
 public class ParallaxBackground : MonoBehaviour
 {
 
-    [SerializeField] private Vector2 parallaxEffectMultiplier;
+    [SerializeField] 
+    private Vector2 parallaxEffectSpeed;
+    [SerializeField] 
+    private GameObject spriteObject;
+    [SerializeField] 
+    private Transform levelPlayerSpawnPoint;
+    
     private Transform cameraTransform;
-    private Vector3 lastCameraPosition;
-    private float textureUnitSizeX;
+    private float startPositionY;
+    private float startPositionX;
+    private float spriteSizeX;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         cameraTransform = Camera.main.transform;
-        lastCameraPosition = cameraTransform.position;
-        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
-        Texture2D texture = sprite.texture;
-        textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
+        startPositionX = levelPlayerSpawnPoint.position.x;
+        startPositionY = transform.position.y;
+        transform.position = new Vector3 (startPositionX, transform.position.y, transform.position.z);
+        spriteSizeX = spriteObject.GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
-        transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y);
-        lastCameraPosition = cameraTransform.position;
 
-        if (Math.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX) {
-            float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
-            transform.position = new Vector3(cameraTransform.position.x + offsetPositionX, transform.position.y);
+        float relativeDistX = cameraTransform.position.x * parallaxEffectSpeed.x;
+        float relativeDistY = cameraTransform.position.y * parallaxEffectSpeed.y;
+        transform.position = new Vector3(startPositionX + relativeDistX, startPositionY + relativeDistY, transform.position.z);
+
+        float relativeCameraDist = cameraTransform.position.x * (1 - parallaxEffectSpeed.x);
+        if (relativeCameraDist > startPositionX + spriteSizeX) {
+            startPositionX += spriteSizeX;
+        }
+        else if (relativeCameraDist < startPositionX - spriteSizeX) {
+            startPositionX -= spriteSizeX;
         }
     }
 }
