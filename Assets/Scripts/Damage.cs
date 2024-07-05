1485435544c124a9a53e20afb8da16d9e67eafbb
Shady Mansour <shady.emad@outlook.com>
2024-07-05 22:35:42 +0200

    fixed bugs, cleaned files, adjusted level 2, added deadzone, adjusted tiles' colliders

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -11,12 +11,26 @@ private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            PlayerController player = other.gameObject.GetComponent<PlayerController>();
-            if (player != null)
-            {
-                // Here you might want to do additional checks or apply effects
-                player.Die();  // Call the die method
-            }
+            KillPlayer(other.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("OnTriggerEnter2D");
+        if (collider.CompareTag("Player"))
+        {
+           KillPlayer(collider.gameObject);
+        }
+    }
+
+    private void KillPlayer(GameObject go)
+    {
+        // Here you might want to do additional checks or apply effects
+        PlayerController player = go.GetComponent<PlayerController>();
+        if (player != null)
+        {
+            player.Die();  // Call the die method
         }
     }
 }
